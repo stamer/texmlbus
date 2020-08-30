d373830dc257519891e180e694210a6ec265e963
Heinrich Stamerjohanns <heinrich@stamerjohanns.de>
2020-08-30 16:49:06 +0200

    Helper script to create a dump of current DB
    - only export table descriptions, no-data
    - export content of help and dbversions table.

---
+++ tools/create-dump.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+SCRIPTDIR=`dirname "$(readlink -f "$0")"`
+PWD=`pwd`
+cd "${SCRIPTDIR}"
+
+DBVERSION=`docker exec texmlbus_mariadb_1 sh -c 'exec mysql buildsysdb -uroot -p"$MYSQL_ROOT_PASSWORD" --silent -e "select dbversion from dbversion"'`
+echo "DB version: ${DBVERSION}"
+LAST_UPGRADE=`ls ../volume/build/config/sql | tail -1`
+echo "Last upgrade file: ${LAST_UPGRADE}"
+echo "Both version should match..."
+sleep 5
+
+OUTFILE='../volume/db/init/buildsysdb.sql'
+echo "Writing backup to ${OUTFILE}"
+
+# create all table, no data
+docker exec texmlbus_mariadb_1 sh -c 'exec mysqldump buildsysdb --no-data -uroot -p"$MYSQL_ROOT_PASSWORD"' > ${OUTFILE}
+
+#dump help contents
+docker exec texmlbus_mariadb_1 sh -c 'exec mysqldump buildsysdb help -uroot -p"$MYSQL_ROOT_PASSWORD"' >> ${OUTFILE}
+
+# dump dbversion for current dbversion
+docker exec texmlbus_mariadb_1 sh -c 'exec mysqldump buildsysdb dbversion -uroot -p"$MYSQL_ROOT_PASSWORD"' >> ${OUTFILE}
+
+# fix AUTOINCREMENT_VALUES
+sed -E -i -e 's/AUTO_INCREMENT=\w+/AUTO_INCREMENT=1/' ${OUTFILE}
+
+echo 'Exported tables:'
+grep "CREATE TABLE" ${OUTFILE} | sed -E -e 's/CREATE TABLE `(.*)` \(/\1/'
+
+cd "${PWD}"
