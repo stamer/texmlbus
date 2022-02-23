7228ebf267d26a18955af9f52e3595e6f00ea7d7
Heinrich Stamerjohanns <heinrich@stamerjohanns.de>
2022-02-23 23:33:50 +0100

    Restrict test to texmlbus.

--- tools/is-dc-v2.sh
+++ tools/is-dc-v2.sh
@@ -4,7 +4,7 @@
 is_dc_v2() {
    # docker-compose v1: _1 at end.
    # docker-compose v2: -1 at end
-   docker container ls --format={{.Names}} | grep '_1$' > /dev/null
+   docker container ls --format={{.Names}} | grep texmlbus | grep '_1$' > /dev/null
    return $?
 }
 
