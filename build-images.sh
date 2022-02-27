2fe4cf1f16bd9c217524935747706aec6486ec95
Heinrich Stamerjohanns <stamer@t3620.fritz.box>
2022-02-27 18:44:25 +0100

    Give explanation, if v2 is not installed.

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,21 @@
+#!/bin/bash
 # Use this script, if you are using docker compose v2
 # and the images do not build.
+#
+# Heinrich Stamerjohanns
+#
+docker compose >/dev/null 2>/dev/null
+if [[ "$?" != "0" ]]; then
+	echo "docker compose v2 has not been setup. ";
+    echo "Either just run "
+    echo
+	echo "docker-compose -f docker-compose.yml -f docker-compose-build.yml build"
+	echo 
+    echo "or read how to install docker compose v2 on"
+    echo "https://github.com/docker/compose/tree/v2#linux"
+	echo 
+    exit 1
+fi
 docker pull alpine:3.12
 docker pull alpine:3.13
 docker compose -f docker-compose.yml -f docker-compose-build.yml build latexml_base
